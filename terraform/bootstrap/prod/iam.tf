6ded198cc158506ab58e7e53f2104ee4909cfa9a
psp0 <tkdvlf9058@g.hongik.ac.kr>
2025-12-13 19:23:35 +0900

    feat: add environment variable to GitHub Actions role for enhanced deployment flexibility

--- terraform/bootstrap/prod/iam.tf
+++ terraform/bootstrap/prod/iam.tf
@@ -343,7 +343,8 @@ resource "aws_iam_role" "github_actions_role" {
           },
           StringLike = {
             "token.actions.githubusercontent.com:sub" = [
-              "repo:${var.full_repo_path}:ref:refs/heads/main"
+              "repo:${var.full_repo_path}:ref:refs/heads/main",
+              "repo:${var.full_repo_path}:environment:${var.environment}"
             ]
           }
         }
